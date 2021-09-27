2f1ec19f7370dd4232c7b763d0b39482b65ec543
balaGit <59569334+grbalaji@users.noreply.github.com>
2021-09-27 14:26:13 +0530

    Add files via upload

---
+++ CustomerMgmt/src/com/tek/CustomerDriver.java
@@ -0,0 +1,13 @@
+package com.tek;
+
+public class CustomerDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		CustomerUtil cu = new CustomerUtil();
+		cu.printCustomers();
+	}
+
+}
